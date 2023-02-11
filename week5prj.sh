ca4c7a1dbea330f56ec91281af6d5200f601b055
EC2 Default User <ec2-user@ip-172-31-57-49.ec2.internal>
2023-02-11 19:12:20 +0000

    first commit of week5 project script

---
+++ week5prj.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "<html>" > /var/www/html/index.html
+echo "<head>" >> /var/www/html/index.html
+echo "  <title>Miguel Camacho's LUIT Gold Team Week 5 Project</title>" >> /var/www/html/index.html
+echo "</head>" >> /var/www/html/index.html
+echo "<body>" >> /var/www/html/index.html
+echo "  <h1>Miguel Camacho's LUIT Gold Team Week 5 Project</h1>" >> /var/www/html/index.html
+echo "  <p>If you're seeing this then Miguel Camacho's LUIT Gold Team Week 5 Project is complete.</p>" >> /var/www/html/index.html
+echo "</body>" >> /var/www/html/index.html
+echo "</html>" >> /var/www/html/index.html
+
