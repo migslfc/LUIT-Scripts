f4da8f956cc6080e83f6374f645869ebf8889668
MIGUEL CAMACHO <ec2-user@ip-172-31-57-49.ec2.internal>
2023-02-11 20:37:40 +0000

    updating script to catch up history

--- week5prj.sh
+++ week5prj.sh
@@ -2,7 +2,7 @@
 
 echo "<html>" > /var/www/html/index.html
 echo "<head>" >> /var/www/html/index.html
-echo "  <title>Miguel Camacho's LUIT Gold Team Week 5 Project</title>" >> /var/www/html/index.html
+echo "  <title>Week 5 Project</title>" >> /var/www/html/index.html
 echo "</head>" >> /var/www/html/index.html
 echo "<body>" >> /var/www/html/index.html
 echo "  <h1>Miguel Camacho's LUIT Gold Team Week 5 Project</h1>" >> /var/www/html/index.html
